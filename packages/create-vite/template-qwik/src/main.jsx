68ad763ded0a8ac4fbaa3ce98bab6bd3e3c1a936
ruijunior549-lab <ruijunior549@gmail.com>
2026-05-26 23:44:53 +0100

    Update main.jsx

--- packages/create-vite/template-qwik/src/main.jsx
+++ packages/create-vite/template-qwik/src/main.jsx
@@ -1,7 +1,9 @@
-import '@builder.io/qwik/qwikloader.js'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-import { render } from '@builder.io/qwik'
-import './index.css'
-import { App } from './app.jsx'
-
-render(document.getElementById('app'), <App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
